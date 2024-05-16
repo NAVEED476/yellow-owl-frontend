89f656c2b9e3061cd95e174f3cf1eff427948193
Naveed Shaik <shaiknaveed435@gmail.com>
2024-05-16 14:56:14 +0530

    updated tsconfig.json file

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import ReactDOM from 'react-dom'; // Assuming you want to import the ReactDOM module
+import App from "./App"
 import './index.css'
 
+// @ts-ignore
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
